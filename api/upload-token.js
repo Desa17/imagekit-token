2d1f9141cca00f22b9f676fb64627e213dc6dc71
Desa17 <t34m0mh17@gmail.com>
2026-02-27 19:24:55 -0500

    Add files via upload

---
+++ api/upload-token.js
@@ -0,0 +1,12 @@
+﻿import ImageKit from "imagekit";
+
+export default function handler(req, res) {
+  const imagekit = new ImageKit({
+    publicKey: "public_d3ArQDMw7/fjt4Z3gsNsViVz8bc=",
+    privateKey: "private_bDA4g97zCLowGA/SdYUcF76JkjY=",
+    urlEndpoint: "https://ik.imagekit.io/tu_cuenta"
+  });
+
+  const authParams = imagekit.getAuthenticationParameters();
+  res.status(200).json(authParams);
+}
